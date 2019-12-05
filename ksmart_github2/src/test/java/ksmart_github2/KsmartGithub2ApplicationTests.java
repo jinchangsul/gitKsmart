a9792ed6c2ccb86c8c6695f8d0cb8cc330dcc64f
Administrator <Administrator@MSDN-SPECIAL>
2019-12-05 11:12:27 +0900

    dfgdgf

---
+++ ksmart_github2/src/test/java/ksmart_github2/KsmartGithub2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ksmart_github2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KsmartGithub2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
